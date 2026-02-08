c6decd426e976da9b8398a34d1b97d80acb066d7
kulbhushan7718-crypto <kulbhushan7718@gmail.com>
2026-02-08 23:54:02 +0530

    Create check.js

---
+++ api/check.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const { address } = req.query;
+
+  const response = await fetch(
+    `https://api.bscscan.com/api?module=contract&action=getsourcecode&address=${address}&apikey=74R7U3DCNSDBBIVGDUDPSUUQ4DJV7STPPR`
+  );
+
+  const data = await response.json();
+  const source = data.result[0].SourceCode;
+
+  let risk = 0;
+
+  if (source.includes("mint(")) risk += 30;
+  if (source.includes("blacklist")) risk += 30;
+  if (source.includes("owner")) risk += 20;
+
+  let result = "Low Risk";
+
+  if (risk >= 60) result = "High Risk";
+  else if (risk >= 30) result = "Medium Risk";
+
+  res.status(200).json({ result });
+}
